49725278b7cc5c2428946814e3eb58e1bc5f7dfc
nannabi <nanee.codes@gmail.com>
2021-01-05 00:33:15 +0900

    clean up index + add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Blog() {
+  return (
+    <Layout>
+      <div>This is my blog~~~</div>
+    </Layout>
+  )
+}
